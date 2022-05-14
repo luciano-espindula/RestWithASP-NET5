2796608a1038ca1903ca6b6d86ba4cef81a839a6
unknown <luc.aespindula@gmail.com>
2022-05-14 20:26:20 -0300

    Connecting To Database and Versioning Endpoints

---
+++ 04_05_RestWithASPNET_Connecting To Database And VersioningEndPoints/RestWithASPNET/RestWithASPNET/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using RestWithASPNET.Model;
+using System.Collections.Generic;
+
+namespace RestWithASPNET.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindByAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
