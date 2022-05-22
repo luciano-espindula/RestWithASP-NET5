b69b13f0333826f8d79bd19f2eff01ffbf374989
unknown <luc.aespindula@gmail.com>
2022-05-22 13:06:42 -0300

    Splitting Logic

---
+++ 06_RestWithASPNET_SplittingLogic/RestWithASPNET/RestWithASPNET/Business/Implementations/PersonBusinessImplementation.cs
@@ -0,0 +1,45 @@
+﻿using RestWithASPNET.Model;
+using RestWithASPNET.Model.Context;
+using RestWithASPNET.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestWithASPNET.Business.Implementations
+{
+    public class PersonBusinessImplementation : IPersonBusiness
+    {
+        private readonly IPersonRepository _repository;
+
+        public PersonBusinessImplementation(IPersonRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public List<Person> FindByAll()
+        {
+            return _repository.FindByAll();
+        }
+
+        public Person FindByID(long id)
+        {
+            return _repository.FindByID(id);
+        }
+
+        public Person Create(Person person)
+        {
+            return _repository.Create(person);
+        }
+
+        public Person Update(Person person)
+        {
+            return _repository.Update(person);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+    }
+}
