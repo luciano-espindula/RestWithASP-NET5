9e85fdf1fc9b918c73676fb10bb2163fe0eb92fd
unknown <luc.aespindula@gmail.com>
2022-05-22 19:51:53 -0300

    Adding Support To Database Migrations

---
+++ 07_RestWithASPNET_AddingSupportToDatabaseMigrations/RestWithASPNET/RestWithASPNET/Repository/IBookRepository.cs
@@ -0,0 +1,15 @@
+﻿using RestWithASPNET.Model;
+using System.Collections.Generic;
+
+namespace RestWithASPNET.Repository
+{
+    public interface IBookRepository
+    {
+        Book Create(Book book);
+        Book FindByID(long id);
+        List<Book> FindByAll();
+        Book Update(Book book);
+        void Delete(long id);
+        bool Exists(long id);
+    }
+}
---
+++ 07_RestWithASPNET_AddingSupportToDatabaseMigrations/RestWithASPNET/RestWithASPNET/Repository/IPersonRepository.cs
@@ -0,0 +1,15 @@
+﻿using RestWithASPNET.Model;
+using System.Collections.Generic;
+
+namespace RestWithASPNET.Repository
+{
+    public interface IPersonRepository
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindByAll();
+        Person Update(Person person);
+        void Delete(long id);
+        bool Exists(long id);
+    }
+}
