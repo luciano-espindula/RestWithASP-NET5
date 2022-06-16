5f1ab39a72ba4690e16d77c40aaf3605f61ff2d9
unknown <luc.aespindula@gmail.com>
2022-06-16 17:39:19 -0300

    Working With Generic Repository and Custom Serialization

---
+++ 09_RestWithASPNET_WorkingWithValueObjects/RestWithASPNET/RestWithASPNET/Controllers/BookController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithASPNET.Model;
+using RestWithASPNET.Business;
+using RestWithASPNET.Data.VO;
+
+namespace RestWithASPNET.Controllers
+{
+    [ApiVersion("1")]
+    [ApiController]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class BookController : ControllerBase
+    {
+
+        private readonly ILogger<BookController> _logger;
+        private IBookBusiness _bookBusiness;
+
+        public BookController(ILogger<BookController> logger, IBookBusiness bookBusiness)
+        {
+            _logger = logger;
+            _bookBusiness = bookBusiness;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_bookBusiness.FindByAll());
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var book = _bookBusiness.FindByID(id);
+            if (book == null) return NotFound();
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] BookVO book)
+        {
+            if (book == null) return BadRequest();
+            return Ok(_bookBusiness.Create(book));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] BookVO book)
+        {
+            if (book == null) return BadRequest();
+            return Ok(_bookBusiness.Update(book));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _bookBusiness.Delete(id);
+            return NoContent();
+        }
+    }
+}
