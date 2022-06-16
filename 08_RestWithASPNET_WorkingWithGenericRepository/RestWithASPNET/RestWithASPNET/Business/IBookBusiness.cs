3917cd653c70b49a5b63d257f1c6c36e50552767
unknown <luc.aespindula@gmail.com>
2022-06-16 16:18:26 -0300

    Working With Generic Repository

---
+++ 08_RestWithASPNET_WorkingWithGenericRepository/RestWithASPNET/RestWithASPNET/Business/IBookBusiness.cs
@@ -0,0 +1,14 @@
+﻿using RestWithASPNET.Model;
+using System.Collections.Generic;
+
+namespace RestWithASPNET.Business
+{
+    public interface IBookBusiness
+    {
+        Book Create(Book book);
+        Book FindByID(long id);
+        List<Book> FindByAll();
+        Book Update(Book book);
+        void Delete(long id);
+    }
+}
---
+++ 08_RestWithASPNET_WorkingWithGenericRepository/RestWithASPNET/RestWithASPNET/Business/Implementations/BookBusinessImplementation.cs
@@ -0,0 +1,42 @@
+﻿using RestWithASPNET.Model;
+using RestWithASPNET.Repository;
+using System.Collections.Generic;
+
+namespace RestWithASPNET.Business.Implementations
+{
+    public class BookBusinessImplementation : IBookBusiness
+    {
+        private readonly IRepository<Book> _repository;
+
+        public BookBusinessImplementation(IRepository<Book> repository)
+        {
+            _repository = repository;
+        }
+
+        public List<Book> FindByAll()
+        {
+            return _repository.FindByAll();
+        }
+
+        public Book FindByID(long id)
+        {
+            return _repository.FindByID(id);
+        }
+
+        public Book Create(Book book)
+        {
+            return _repository.Create(book);
+        }
+
+        public Book Update(Book book)
+        {
+            return _repository.Update(book);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+    }
+}
