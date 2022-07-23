194af535885720111c20dbecb9ad7fc590364c3a
unknown <luc.aespindula@gmail.com>
2022-07-23 20:08:54 -0300

    Working with HATEOAS

---
+++ 12_RestWithASPNET_ HATEOAS/RestWithASPNET/RestWithASPNET/Controllers/PersonController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithASPNET.Model;
+using RestWithASPNET.Business;
+using RestWithASPNET.Data.VO;
+using RestWithASPNET.Hypermedia.Filters;
+
+namespace RestWithASPNET.Controllers
+{
+    [ApiVersion("1")]
+    [ApiController]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class PersonController : ControllerBase
+    {
+
+        private readonly ILogger<PersonController> _logger;
+        private IPersonBusiness _personBusiness;
+
+        public PersonController(ILogger<PersonController> logger, IPersonBusiness personBusiness)
+        {
+            _logger = logger;
+            _personBusiness = personBusiness;
+        }
+
+        [HttpGet]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get()
+        {
+            return Ok(_personBusiness.FindByAll());
+
+        }
+
+        [HttpGet("{id}")]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get(long id)
+        {
+            var person = _personBusiness.FindByID(id);
+            if (person == null) return NotFound();
+            return Ok(person);
+        }
+
+        [HttpPost]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Post([FromBody] PersonVO person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personBusiness.Create(person));
+        }
+
+        [HttpPut]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Put([FromBody] PersonVO person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personBusiness.Update(person));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _personBusiness.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ 12_RestWithASPNET_ HATEOAS/RestWithASPNET/RestWithASPNET/Repository/Generic/GenericRepository.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestWithASPNET.Model.Base;
+using RestWithASPNET.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestWithASPNET.Repository.Generic
+{
+    public class GenericRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        private MySQLContext _context;
+        private DbSet<T> dataset;
+
+        public GenericRepository(MySQLContext context)
+        {
+            _context = context;
+            dataset = _context.Set<T>();
+        }
+
+        public List<T> FindByAll()
+        {
+            return dataset.ToList();
+        }
+
+        public T FindByID(long id)
+        {
+            return dataset.SingleOrDefault(p => p.Id.Equals(id));
+        }
+
+        public T Create(T item)
+        {
+            try
+            {
+                dataset.Add(item);
+                _context.SaveChanges();
+                return item;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public T Update(T item)
+        {
+            var result = FindByID(item.Id);
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(item);
+                    _context.SaveChanges();
+                    return result;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void Delete(long id)
+        {
+            var result = FindByID(id);
+            if (result != null)
+            {
+                try
+                {
+                    dataset.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        public bool Exists(long id)
+        {
+            return dataset.Any(p => p.Id.Equals(id));
+        }
+    }
+}
