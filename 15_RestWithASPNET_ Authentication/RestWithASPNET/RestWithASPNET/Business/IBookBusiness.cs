5018645380ee12601685103249ea2434286e2c74
unknown <luc.aespindula@gmail.com>
2022-10-12 14:37:01 -0300

    Adding Authentication Support

---
+++ 15_RestWithASPNET_ Authentication/RestWithASPNET/RestWithASPNET/Business/IBookBusiness.cs
@@ -0,0 +1,14 @@
+﻿using RestWithASPNET.Data.VO;
+using System.Collections.Generic;
+
+namespace RestWithASPNET.Business
+{
+    public interface IBookBusiness
+    {
+        BookVO Create(BookVO book);
+        BookVO FindByID(long id);
+        List<BookVO> FindByAll();
+        BookVO Update(BookVO book);
+        void Delete(long id);
+    }
+}
---
+++ 15_RestWithASPNET_ Authentication/RestWithASPNET/RestWithASPNET/Data/VO/TokenVO.cs
@@ -0,0 +1,20 @@
+﻿namespace RestWithASPNET.Data.VO
+{
+    public class TokenVO
+    {
+        public TokenVO(bool authentication, string created, string expiration, string accessToken, string refreshToken)
+        {
+            Authentication = authentication;
+            Created = created;
+            Expiration = expiration;
+            AccessToken = accessToken;
+            RefreshToken = refreshToken;
+        }
+
+        public bool Authentication { get; set; }
+        public string Created { get; set; }
+        public string Expiration { get; set; }
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
