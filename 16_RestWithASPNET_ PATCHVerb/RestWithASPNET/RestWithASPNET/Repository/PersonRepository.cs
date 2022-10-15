dab26ed1d1f5eea17d064b00eb7a8300e54cd595
unknown <luc.aespindula@gmail.com>
2022-10-15 20:10:01 -0300

    Working with PATCH Verb

---
+++ 16_RestWithASPNET_ PATCHVerb/RestWithASPNET/RestWithASPNET/Repository/PersonRepository.cs
@@ -0,0 +1,35 @@
+﻿using RestWithASPNET.Model;
+using RestWithASPNET.Model.Context;
+using RestWithASPNET.Repository.Generic;
+using System;
+using System.Linq;
+
+namespace RestWithASPNET.Repository
+{
+    public class PersonRepository : GenericRepository<Person>, IPersonRepository
+    {
+        public PersonRepository(MySQLContext context) : base(context) { }
+
+        public Person Disable(long id)
+        {
+            if (!_context.Persons.Any(p => p.Id.Equals(id))) return null;
+
+            var person = _context.Persons.FirstOrDefault(p => p.Id.Equals(id));
+            if (person != null)
+            {
+                person.Enabled = false;
+                try
+                {
+                    _context.Entry(person).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                
+            }
+            return person;
+        }
+    }
+}
