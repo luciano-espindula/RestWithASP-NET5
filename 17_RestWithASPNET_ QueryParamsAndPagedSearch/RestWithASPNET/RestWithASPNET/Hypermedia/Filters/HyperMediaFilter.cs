2273ee62c5a822751da8e369f05df34032b8fb26
unknown <luc.aespindula@gmail.com>
2022-11-03 19:21:55 -0300

    Working with Query Params and Paged Search

---
+++ 17_RestWithASPNET_ QueryParamsAndPagedSearch/RestWithASPNET/RestWithASPNET/Hypermedia/Filters/HyperMediaFilter.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Hypermedia.Filters
+{
+    public class HyperMediaFilter: ResultFilterAttribute
+    {
+        private HyperMediaFilterOptions _hiperMediaFilterOptions;
+
+        public HyperMediaFilter(HyperMediaFilterOptions hyperMediaFilterOptions)
+        {
+            _hiperMediaFilterOptions = hyperMediaFilterOptions;
+        }
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            TryEnrichResult(context);
+            base.OnResultExecuting(context);
+        }
+
+        private void TryEnrichResult(ResultExecutingContext context)
+        {
+            if (context.Result is OkObjectResult objectResult)
+            {
+                var enricher = _hiperMediaFilterOptions.ContentResponseEnricherList.FirstOrDefault(x => x.CanEnrich(context));
+
+                if (enricher != null) Task.FromResult(enricher.Enrich(context));
+            }
+        }
+    }
+}
---
+++ 17_RestWithASPNET_ QueryParamsAndPagedSearch/RestWithASPNET/RestWithASPNET/Repository/PersonRepository.cs
@@ -0,0 +1,55 @@
+﻿using RestWithASPNET.Model;
+using RestWithASPNET.Model.Context;
+using RestWithASPNET.Repository.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestWithASPNET.Repository
+{
+    public class PersonRepository : GenericRepository<Person>, IPersonRepository
+    {
+        public PersonRepository(MySQLContext context) : base(context) { }
+
+        public Person Disable(long id)
+        {
+            if (!_context.Persons.Any(p => p.Id.Equals(id))) return null;
+
+            var person = _context.Persons.FirstOrDefault(p => p.Id.Equals(id));
+            if (person != null)
+            {
+                person.Enabled = false;
+                try
+                {
+                    _context.Entry(person).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                
+            }
+            return person;
+        }
+
+        public List<Person> FindByName(string firstName, string lastName)
+        {
+            if (!string.IsNullOrWhiteSpace(firstName) && !string.IsNullOrWhiteSpace(lastName))
+            {
+                return _context.Persons.Where(p => p.FirstName.Contains(firstName)
+                    && p.LastName.Contains(lastName)).ToList();
+            }
+            else if (!string.IsNullOrWhiteSpace(firstName) && string.IsNullOrWhiteSpace(lastName))
+            {
+                return _context.Persons.Where(p => p.FirstName.Contains(firstName)).ToList();
+            }
+            else if (string.IsNullOrWhiteSpace(firstName) && !string.IsNullOrWhiteSpace(lastName))
+            {
+                return _context.Persons.Where(p => p.LastName.Contains(lastName)).ToList();
+            }
+
+            return null;
+        }
+    }
+}
